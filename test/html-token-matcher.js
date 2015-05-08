407f0c6cf7abe3bd47b225382c8eedf2afb83776
Sergey Chikuyonok <serge.che@gmail.com>
2015-05-09 00:29:11 +0300

    Working HTML token streaming matcher

--- test/html-token-matcher.js
+++ test/html-token-matcher.js
@@ -18,24 +18,83 @@ describe('HTML Token Matcher', function() {
 		assert.equal(m.search('<!-- </head> --> </body>'), 17);
 	});
 
-	it.only('performance', function() {
-		var fs = require('fs');
-		var path = require('path');
-		var buf = fs.readFileSync(path.join(__dirname, 'test.html'));
-
-		var start2 = Date.now();
-		var _m2 = buf.toString('utf8').match(/<\/(head|body|html)>/i);
-		var m2 = _m2.index;
-		var end2 = Date.now();
-
+	it('feed match', function() {
 		var m = htmlMatcher();
-		var start = Date.now();
-		var m1 = m.search(buf);
-		var end = Date.now();
+		assert.equal(m.search('hello </bo'), -1);
+		assert.equal(m.offset, 6);
+		assert.equal(m.search('hello </bod'), -1);
+		assert.equal(m.offset, 6);
+		assert.equal(m.search('hello </body'), -1);
+		assert.equal(m.offset, 6);
+		assert.equal(m.search('hello </body> test'), 6);
 
-		
+		m.reset();
+		assert.equal(m.search('a <!'), -1);
+		assert.equal(m.offset, 2);
+		assert.equal(m.search('a <!--'), -1);
+		assert.equal(m.offset, 6);
+		assert.equal(m.search('a <!-- </head>'), -1);
+		assert.equal(m.offset, 14);
+		assert.equal(m.search('a <!-- </head> --'), -1);
+		assert.equal(m.offset, 15);
+		assert.equal(m.search('a <!-- </head> --> </head>'), 19);
 
-		console.log('First match: %d @ %dms', m1, end - start);
-		console.log('Second match: %d @ %dms', m2, end2 - start2);
+		// test optimistic offset
+		m.reset();
+		assert.equal(m.search('<!'), -1);
+		assert.equal(m.offset, 0);
+		assert.equal(m.search('<!-- foo -'), -1);
+		assert.equal(m.offset, 9);
+		assert.equal(m.search('<!-- foo - bar -'), -1);
+		assert.equal(m.offset, 9);
+		assert.equal(m.search('<!-- foo - bar --></head>'), 18);
+	});
+
+	it('feed-shift match', function() {
+		var m = htmlMatcher();
+		var input = new Buffer('');
+		var output = [];
+		var pos = 0;
+
+		var push = function(chunk) {
+			input = Buffer.concat([input, new Buffer(chunk)]);
+		};
+		var shift = function() {
+			var chunk = m.shift(input);
+			if (!chunk.length) {
+				return;
+			}
+			pos += chunk.length;
+			output.push(chunk);
+			if (chunk === input) {
+				input = new Buffer('');
+			} else {
+				input = input.slice(chunk.length);
+			}
+		};
+
+		var feed = function(chunk) {
+			push(chunk);
+			var res = m.search(input);
+			shift();
+			return res;
+		};
+
+		assert.equal(feed('foo'), -1);
+		assert.equal(feed('<!'), -1);
+		assert.equal(feed('-- bar'), -1);
+		assert.equal(feed('-->'), -1);
+		assert.equal(feed('</a'), -1);
+		assert.equal(feed('></he'), -1);
+		assert.equal(feed('ad>'), 0);
+
+		output.push(input);
+
+		output = output.map(function(b) {
+			return b.toString();
+		}).join('');
+
+		assert.equal(output, 'foo<!-- bar--></a></head>');
+		assert.equal(pos, 18);
 	});
 });
