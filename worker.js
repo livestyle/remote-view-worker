41ae8004938f8b79401eaf3e0c0c329f8db613cf
Sergey Chikuyonok <serge.che@gmail.com>
2015-06-02 01:25:30 +0300

    Working on DB-connected session manager

---
+++ worker.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env iojs
+'use strict';
+
+var mongo = require('mongodb').MongoClient;
+var server = require('./lib/server');
+var sessionManager = require('./lib/session-manager');
+
+var mongoUrl = process.env.RV_MONGO_DB || 'mongodb://localhost:27017/rv';
+
+mongo.connect(function(err, db) {
+	if (err) {
+		throw err;
+	}
+
+	console.log('has unref?', db.unref);
+	sessionManager.setup(db, {
+		requestTimeout: 10000
+	});
+	
+	server(function() {
+		console.log('Remote View worker is up and running on %d', this.address().port);
+	});
+});
+
+
