852f5d877835076c45a7aa7ac742707969902e65
Sergey Chikuyonok <serge.che@gmail.com>
2015-06-17 19:45:42 +0300

    Start worker with test session

--- worker.js
+++ worker.js
@@ -12,12 +12,37 @@ mongo.connect(mongoUrl, function(err, db) {
 		throw err;
 	}
 
-	console.log('has unref?', db.unref);
-	sessionManager.setup(db, {
-		requestTimeout: 10000
-	});
-	
+	sessionManager.setup(db);
 	server(function() {
 		console.log('Remote View worker is up and running on %d', this.address().port);
+		var ix = process.argv.indexOf('--test');
+		if (ix) {
+			// create a test session for given url
+			var sessionId = '__test-session';
+			var localSite = process.argv[ix + 1] || 'http://localhost:8080';
+			db.collection('Session').update({_id: sessionId}, {$set: {
+				_id: sessionId,
+				user: 0,
+				publicId: 'rv-test',
+				localSite: localSite,
+				created: Date.now(),
+				expiresAt: Date.now() + 24 * 60 * 60 * 1000,
+				active: true
+			}}, {upsert: true}, function(err) {
+				if (err) {
+					throw err;
+				}
+
+				console.log('Created temp session with id %s for local site %s', sessionId, localSite);
+			});
+
+			process.on('SIGINT', function() {
+				db.collection('Session').deleteOne({_id: sessionId}, function() {
+					console.log('removed temp session');
+					db.close();
+					process.exit();
+				});
+			});
+		}
 	});
 });
