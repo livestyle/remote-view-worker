43992b313680fd0ff16e95830fb49e8e1979fd70
Sergey Chikuyonok <serge.che@gmail.com>
2015-06-25 01:47:41 +0300

    Removed ack wait response from tunnel
    Added some performance metricks

--- perf.js
+++ perf.js
@@ -8,6 +8,7 @@ var server = require('./lib/server');
 var sessionManager = require('./lib/session-manager');
 
 var mongoUrl = process.env.RV_MONGO_DB || 'mongodb://localhost:27017/rv';
+var curServer;
 
 mongo.connect(mongoUrl, function(err, db) {
 	if (err) {
@@ -15,7 +16,7 @@ mongo.connect(mongoUrl, function(err, db) {
 	}
 
 	sessionManager.setup(db);
-	server(function() {
+	curServer = server(function() {
 		console.log('Remote View worker is up and running on %d', this.address().port);
 		// create a test session for given url
 		var sessionId = '__test-session';
@@ -35,7 +36,7 @@ mongo.connect(mongoUrl, function(err, db) {
 
 			console.log('Created temp session with id %s for local site %s', sessionId, localSite);
 
-			setInterval(stats, 1000);
+			setInterval(stats, 300).unref();
 		});
 
 		http.createServer(function(req, res) {
@@ -51,13 +52,23 @@ charm.reset();
 function stats() {
 	// draw simple dashboard
 	var sessionIds = sessionManager.activeSessions();
-
 	var dy = 1;
+	var line = function(msg) {
+		charm.position(1, dy++).write(msg);
+	};
 	charm.erase('screen').cursor(false);
 	sessionIds.forEach(function(id) {
 		sessionManager.getSession(id).then(function(session) {
-			charm.position(1, dy++)
-			.write(`${id}: opened: ${session.sockets.length}, pending: ${session._queue.length}, served: ${session._socketId}`);
+			line(`${id}: opened: ${session.sockets.length}, pending: ${session._queue.length}, served: ${session._socketId}`);
 		});
+	});
+	line(`Opened server sockets: ${server.openedSockets()}`);
+	line(`Server stats:`);
+	Object.keys(server.stats).forEach(function(key) {
+		line(`   ${key}: ${server.stats[key]}`);
+	});
+
+	curServer.getConnections(function(err, count) {
+		line('Actual connections: ' + count);
 	});
 }
